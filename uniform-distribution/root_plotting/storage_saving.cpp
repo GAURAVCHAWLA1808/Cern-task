6a76ca94edffa746c0c2af88813aa9be9a6293a3
Gaurav Chawla <chawlagaurav1805@gmail.com>
2025-03-16 16:00:37 +0530

    detailed analysis of gaussian and exponential destribution is added and updated readme.md and task-report

---
+++ uniform-distribution/root_plotting/storage_saving.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <TCanvas.h>
+#include <TGraph.h>
+#include <TAxis.h>
+#include <TLatex.h>
+
+const int numTechniques = 7;
+const char* techniques[numTechniques] = {
+    "No Compression", "gzip", "8-bit Zeroing + gzip","10-bit Zeroing + gzip",
+    "12-bit Zeroing + gzip", "16-bit Zeroing + gzip", "32-bit to 16-bit"
+};
+
+double storage_savings[numTechniques] = {0, 10.2, 28.6, 35.04,43.8, 58.1, 50.0};
+
+// Storage Savings vs Techniques
+void plotStorageSavings() {
+    TCanvas* c1 = new TCanvas("c1", "Storage Savings vs Techniques", 800, 600);
+    TGraph* graph = new TGraph(numTechniques);
+
+    for (int i = 0; i < numTechniques; i++) {
+        graph->SetPoint(i, i, storage_savings[i]);
+    }
+
+    graph->SetTitle("Storage Savings (%) vs. Compression Technique");
+    graph->GetXaxis()->SetTitle("Compression Technique");
+    graph->GetYaxis()->SetTitle("Storage Savings (%)");
+    graph->SetMarkerStyle(20);
+    graph->SetMarkerColor(kBlue);
+    graph->Draw("APL");
+
+    for (int i = 0; i < numTechniques; i++) {
+        TLatex* tex = new TLatex(i + 0.1, storage_savings[i] + 2, techniques[i]);  // Shift right
+        tex->SetTextSize(0.03);
+        tex->Draw();
+    }
+
+    c1->SaveAs("storage_savings.png");
+}
+
+int main() {
+    plotStorageSavings();
+    return 0;
+}
